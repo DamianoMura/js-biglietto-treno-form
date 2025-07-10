b6e48996e7359d61a5e247d8b00649fe73a89c81
DamianoMura <mura.damiano14@gmail.com>
2025-07-10 18:09:12 +0200

    [upd] found values by id

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
+// ## Descrizione:
+
+// Scrivere un programma che chieda all’utente:
+// Il numero di chilometri da percorrere,
+// Età del passeggero,
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di
+// viaggio, secondo le seguenti regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km),
+// va applicato uno sconto del 20% per i minorenni,
+// va applicato uno sconto del 40% per gli over 65.
 // # - MILESTONE 1:
 
 // Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
 
+// il contenuto del nostro primo text-input di id# full-name lo troviamo : 
+console.log(document.getElementById(`full-name`).value)
+// il contenuto del secondo text-input di id# distance lo troviamo :
+console.log(document.getElementById(`distance`).value)
+// il contenuto del secondo text-input di id# distance lo troviamo :
+console.log(document.getElementById(`age-range`).value)
